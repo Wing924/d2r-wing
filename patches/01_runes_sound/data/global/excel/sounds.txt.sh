98f4b88e1b699d112a5cc07687f9a117855a560b
Wing924 <weihe924stephen@gmail.com>
2022-06-12 22:08:04 +0900

    fix rune sound

--- patches/01_runes_sound/data/global/excel/sounds.txt.sh
+++ patches/01_runes_sound/data/global/excel/sounds.txt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cat
-printf "high_rune	11385		sfx/items_hd	item\high_rune.flac	0	0	0	0	0	255	255	100	100	0	0	0	0	0	0	1	0	0	1	0	90	100	0	0	0	0	0	-1	-1	-1	0	0"
+printf "%s\r\n" "high_rune	11385		sfx/items_hd	item\high_rune.flac	0	0	0	0	0	255	255	100	100	0	0	0	0	0	0	1	0	0	1	0	90	100	0	0	0	0	0	-1	-1	-1	0	0	"
