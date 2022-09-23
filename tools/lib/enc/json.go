c139c8a0b2d483e5b79f0fc1669c7fb97ebf5c51
Wei He <weihe924stephen@gmail.com>
2022-09-23 21:33:26 +0900

    update origin to 2.5 (#30)

--- tools/lib/enc/json.go
+++ tools/lib/enc/json.go
@@ -3,10 +3,16 @@ package enc
 import (
 	"encoding/json"
 	"github.com/Wing924/d2r-wing/tools/lib/model"
+	"github.com/tailscale/hujson"
 )
 
 func ReadStringsJSON(filename string) []model.Entry {
 	content := ReadFileWithBOM(filename)
+	var err error
+	content, err = hujson.Standardize(content)
+	if err != nil {
+		panic(err)
+	}
 	var result []model.Entry
 	if err := json.Unmarshal(content, &result); err != nil {
 		panic(err)
