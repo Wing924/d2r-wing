b69e191aa0720d4d41093873901e60a1f1686ab3
Wei He <weihe924stephen@gmail.com>
2022-11-21 13:02:34 +0900

    修复了投掷武器不显示数量的问题 (#50)

--- patches/13_base_item_desc/data/global/excel/weapons.txt.sh
+++ patches/13_base_item_desc/data/global/excel/weapons.txt.sh
@@ -2,15 +2,19 @@
 
 awk -F"\t" -v OFS="\t" '
 $1 == "name" {
-    print substr($0, 0, length($0)-1), "spelldesc", "spelldescstr", "spelldescstr2"
+    print substr($0, 0, length($0)-1), "spelldesc", "spelldescstr", "spelldescstr2", "spelldesccalc"
     next
 }
 $1 == "Expansion" {
-    print substr($0, 0, length($0)-1), "", "", ""
+    print substr($0, 0, length($0)-1), "", "", "", ""
+    next
+}
+$47 == "1" {
+    print substr($0, 0, length($0)-1), 2, "base_info_" $4, "", ""
     next
 }
 {
-    print substr($0, 0, length($0)-1), 1, "base_info_" $4, ""
+    print substr($0, 0, length($0)-1), 1, "base_info_" $4, "", ""
     next
 }
 '
